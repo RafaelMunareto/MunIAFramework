b2a30a8bd07af63a0cba8f4ff5d9bd0e7106c333
Rafael Munareto <rafael.munareto@icloud.com>
2024-01-06 18:50:25 -0300

    ajustes

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-pip install virtualenv
-virtualenv env --python=python3.12.1
-source env/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
-python controller/app.py
